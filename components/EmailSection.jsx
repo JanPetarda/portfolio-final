1b8e59c617372eb7850db79cd1871079b3eab1d4
JanPetarda <84961505+JanPetarda@users.noreply.github.com>
2024-01-06 13:13:31 +0100

    Placeholder changes

--- components/EmailSection.jsx
+++ components/EmailSection.jsx
@@ -83,7 +83,7 @@ const EmailSection = () => {
                 id="email"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="jacob@google.com"
+                placeholder="name@email.com"
               />
             </div>
             <div className="mb-6">
@@ -99,7 +99,7 @@ const EmailSection = () => {
                 id="subject"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Just saying hi"
+                placeholder="Subject"
               />
             </div>
             <div className="mb-6">
